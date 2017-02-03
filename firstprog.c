f8d1c0ca2c486804a2216719c1051743e15e538a
n1cfury <fukugetin@gmail.com>
2017-02-03 15:07:43 -0800

    My first program in C.  Simple Hello World

---
+++ firstprog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i=0; i < 10; i++)				//Loop 10 times.
+	{
+		printf("Hello World!\n");		//put the string to the output
+	}
+	return 0;							//Tell OS the program exited without errors.
+}
